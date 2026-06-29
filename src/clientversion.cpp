eec3057d334cd2d66102d03f64df40217ddf1809
Capri <caprimc@yahoo.com>
2026-06-29 21:11:14 +0000

    Rename client and binaries to Vexta

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2012-202 The Bitcoin Core developers
-// Copyright (c) 2014-2020 The DigiByte Core developers
+// Copyright (c) 2014-2020 The Vexta Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -10,10 +10,10 @@
 
 /**
  * Name of client reported in the 'version' message. Report the same name
- * for both digibyted and digibyte-qt, to make it harder for attackers to
+ * for both vextad and digibyte-qt, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("DigiByte");
+const std::string CLIENT_NAME("Vexta");
 
 
 #ifdef HAVE_BUILD_INFO
