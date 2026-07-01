b501ad786e4e50a23e7a1bb194a416914c101a63
Capri <caprimc@yahoo.com>
2026-07-01 21:48:38 +0000

    Remove redundant SHA256D block version assignment

--- src/miner.cpp
+++ src/miner.cpp
@@ -122,8 +122,6 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
     assert(pindexPrev != nullptr);
     nHeight = pindexPrev->nHeight + 1;
     pblock->nVersion = g_versionbitscache.ComputeBlockVersion(pindexPrev, chainparams.GetConsensus());
-    pblock->nVersion &= ~BLOCK_VERSION_ALGO;
-    pblock->nVersion |= BLOCK_VERSION_SHA256D;
   
     // -regtest only: allow overriding block.nVersion with
     // -blockversion=N to test forking scenarios
