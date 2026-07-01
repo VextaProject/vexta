6eadfc9f125a02b265010d35d85cbfc365da630d
Capri <caprimc@yahoo.com>
2026-07-01 16:51:11 +0000

    Remove remaining Vexta legacy mining hash references

--- src/test/crypto_tests.cpp
+++ src/test/crypto_tests.cpp
@@ -5,7 +5,6 @@
 
 #include <crypto/aes.h>
 #include <crypto/chacha20.h>
-#include <crypto/odocrypt.h>
 #include <crypto/chacha_poly_aead.h>
 #include <crypto/hkdf_sha256_32.h>
 #include <crypto/hmac_sha256.h>
