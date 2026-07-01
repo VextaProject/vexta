7fb78d8bbaba2079701f03d559205f14e448b8c0
Capri <caprimc@yahoo.com>
2026-07-01 13:33:13 +0000

    Simplify Vexta difficulty retargeting

--- src/pow.cpp
+++ src/pow.cpp
@@ -27,62 +27,35 @@ unsigned int InitialDifficulty(const Consensus::Params& params, int algo)
 
 unsigned int GetNextWorkRequiredVexta(const CBlockIndex* pindexLast, const Consensus::Params& params, int algo)
 {
-    // find first block in averaging interval
-    // Go back by what we want to be nAveragingInterval blocks per algo
+    // Vexta is SHA256D-only. Use one global averaging window.
+    if (pindexLast == nullptr) {
+        return InitialDifficulty(params, ALGO_SHA256D);
+    }
+
     const CBlockIndex* pindexFirst = pindexLast;
-    for (int i = 0; pindexFirst && i < NUM_ALGOS*params.nAveragingInterval; i++)
-    {
+    for (int i = 0; pindexFirst && i < params.nAveragingInterval; i++) {
         pindexFirst = pindexFirst->pprev;
     }
 
-    const CBlockIndex* pindexPrevAlgo = GetLastBlockIndexForAlgoFast(pindexLast, params, algo);
-    if (pindexPrevAlgo == nullptr || pindexFirst == nullptr)
-    {
-        return InitialDifficulty(params, algo);
+    if (pindexFirst == nullptr) {
+        return InitialDifficulty(params, ALGO_SHA256D);
     }
 
-    // Limit adjustment step
-    // Use medians to prevent time-warp attacks
-    int64_t nActualTimespan = pindexLast-> GetMedianTimePast() - pindexFirst->GetMedianTimePast();
-    nActualTimespan = params.nAveragingTargetTimespanV4 + (nActualTimespan - params.nAveragingTargetTimespanV4)/4;
+    int64_t nActualTimespan = pindexLast->GetMedianTimePast() - pindexFirst->GetMedianTimePast();
+    nActualTimespan = params.nAveragingTargetTimespanV4 + (nActualTimespan - params.nAveragingTargetTimespanV4) / 4;
 
     if (nActualTimespan < params.nMinActualTimespanV4)
         nActualTimespan = params.nMinActualTimespanV4;
     if (nActualTimespan > params.nMaxActualTimespanV4)
         nActualTimespan = params.nMaxActualTimespanV4;
 
-    //Global retarget
     arith_uint256 bnNew;
-    bnNew.SetCompact(pindexPrevAlgo->nBits);
+    bnNew.SetCompact(pindexLast->nBits);
 
     bnNew *= nActualTimespan;
     bnNew /= params.nAveragingTargetTimespanV4;
 
-    //Per-algo retarget
-    int nAdjustments = pindexPrevAlgo->nHeight + NUM_ALGOS - 1 - pindexLast->nHeight;
-    if (nAdjustments > 0)
-    {
-        for (int i = 0; i < nAdjustments; i++)
-        {
-            bnNew *= 100;
-            bnNew /= (100 + params.nLocalTargetAdjustment);
-        }
-    }
-    else if (nAdjustments < 0)//make it easier
-    {
-        for (int i = 0; i < -nAdjustments; i++)
-        {
-            bnNew *= (100 + params.nLocalTargetAdjustment);
-            bnNew /= 100;
-            if (bnNew > UintToArith256(params.powLimit)) {
-              bnNew = UintToArith256(params.powLimit);
-              break;
-            }            
-        }
-    }
-
-    if (bnNew > UintToArith256(params.powLimit))
-    {
+    if (bnNew > UintToArith256(params.powLimit)) {
         bnNew = UintToArith256(params.powLimit);
     }
 
