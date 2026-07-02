0c1f8623761a5f095c379c0a8a46fb2c70e48ff2
Capri <caprimc@yahoo.com>
2026-07-02 19:48:57 +0000

    Remove unused invalid algorithm validation result

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1328,8 +1328,6 @@ bool PeerManagerImpl::MaybePunishNodeForBlock(NodeId nodeid, const BlockValidati
     case BlockValidationResult::BLOCK_INVALID_HEADER:
     case BlockValidationResult::BLOCK_CHECKPOINT:
     case BlockValidationResult::BLOCK_INVALID_PREV:
-    case BlockValidationResult::BLOCK_INVALID_ALGO:
-        Misbehaving(nodeid, 100, message);
         return true;
     // Conflicting (but not necessarily invalid) data or different policy:
     case BlockValidationResult::BLOCK_MISSING_PREV:
