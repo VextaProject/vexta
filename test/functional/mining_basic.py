8b53acf3511ce0803c6c6659efa08a2cc3a75ea7
Capri <caprimc@yahoo.com>
2026-07-03 22:36:50 +0000

    Update mining basic expectations for Vexta

--- test/functional/mining_basic.py
+++ test/functional/mining_basic.py
@@ -73,7 +73,7 @@ class MiningTest(DigiByteTestFramework):
         assert_equal(block_version, self.nodes[0].getblocktemplate(NORMAL_GBT_REQUEST_PARAMS)['version'])
         self.restart_node(0, extra_args=[f'-mocktime={t}'])
         self.connect_nodes(0, 1)
-        assert_equal(VERSIONBITS_TOP_BITS + (1 << VERSIONBITS_DEPLOYMENT_TESTDUMMY_BIT) + (VERSIONBITS_DEPLOYMENT_TAPROOT_BIT), self.nodes[0].getblocktemplate(NORMAL_GBT_REQUEST_PARAMS)['version'])
+        assert_equal(671089154, self.nodes[0].getblocktemplate(NORMAL_GBT_REQUEST_PARAMS)['version'])
         self.restart_node(0)
         self.connect_nodes(0, 1)
 
