0a6adef2b7c1c1c447d8a24e902f3fd55914cf64
Capri <caprimc@yahoo.com>
2026-07-03 18:31:29 +0000

    Use wallet generated addresses for functional test cache

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -778,7 +778,7 @@ class DigiByteTestFramework(metaclass=DigiByteTestMetaClass):
             # block in the cache does not age too much (have an old tip age).
             # This is needed so that we are out of IBD when the test starts,
             # see the tip age check in IsInitialBlockDownload().
-            gen_addresses = [k.address for k in TestNode.PRIV_KEYS][:3] + [ADDRESS_BCRT1_P2WSH_OP_TRUE]
+            gen_addresses = [self.nodes[0].getnewaddress() for _ in range(4)]
             assert_equal(len(gen_addresses), 4)
             for i in range(8):
                 self.generatetoaddress(
