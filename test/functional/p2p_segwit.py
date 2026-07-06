0f93a0d18061f38738637e16e678b02329f8287a
Capri <caprimc@yahoo.com>
2026-07-06 12:56:54 +0000

    Update P2P segwit test for Vexta address prefix

--- test/functional/p2p_segwit.py
+++ test/functional/p2p_segwit.py
@@ -2045,7 +2045,7 @@ class SegWitTest(DigiByteTestFramework):
 
         self.nodes[0].sendtoaddress(self.nodes[0].getnewaddress(address_type='bech32'), 5)
         self.generate(self.nodes[0], 1)
-        unspent = next(u for u in self.nodes[0].listunspent() if u['spendable'] and u['address'].startswith('dgbrt'))
+        unspent = next(u for u in self.nodes[0].listunspent() if u['spendable'] and u['address'].startswith('vtxrt'))
 
         raw = self.nodes[0].createrawtransaction([{"txid": unspent['txid'], "vout": unspent['vout']}], {self.nodes[0].getnewaddress(): 1})
         tx = tx_from_hex(raw)
