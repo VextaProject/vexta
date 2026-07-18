e720b1a3cc523078cf92a9c7d60a1a09cbaf0570
Capri <caprimc@yahoo.com>
2026-07-18 19:18:52 +0000

    Update wallet and RPC branding

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -2079,7 +2079,7 @@ static RPCHelpMan encryptwallet()
                 RPCExamples{
             "\nEncrypt your wallet\n"
             + HelpExampleCli("encryptwallet", "\"my pass phrase\"") +
-            "\nNow set the passphrase to use the wallet, such as for signing or sending digibyte\n"
+            "\nNow set the passphrase to use the wallet, such as for signing or sending Vexta\n"
             + HelpExampleCli("walletpassphrase", "\"my pass phrase\"") +
             "\nNow we can do something like sign\n"
             + HelpExampleCli("signmessage", "\"address\" \"test message\"") +
