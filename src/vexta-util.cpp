d322b87d6fee60fa482a700f9f23ce2ce7a3a084
Capri <caprimc@yahoo.com>
2026-07-18 22:46:06 +0000

    Rename Vexta utility setup function

--- src/vexta-util.cpp
+++ src/vexta-util.cpp
@@ -28,7 +28,7 @@ static const int CONTINUE_EXECUTION=-1;
 
 const std::function<std::string(const char*)> G_TRANSLATION_FUN = nullptr;
 
-static void SetupDigiByteUtilArgs(ArgsManager &argsman)
+static void SetupVextaUtilArgs(ArgsManager &argsman)
 {
     SetupHelpOptions(argsman);
 
@@ -43,7 +43,7 @@ static void SetupDigiByteUtilArgs(ArgsManager &argsman)
 // CONTINUE_EXECUTION when it's expected to continue further.
 static int AppInitUtil(ArgsManager& args, int argc, char* argv[])
 {
-    SetupDigiByteUtilArgs(args);
+    SetupVextaUtilArgs(args);
     std::string error;
     if (!args.ParseParameters(argc, argv, error)) {
         tfm::format(std::cerr, "Error parsing command line arguments: %s\n", error);
