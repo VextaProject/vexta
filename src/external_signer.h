2bb11bcd0409f74d8e9f45d1bedd4d2cad89f24f
Capri <caprimc@yahoo.com>
2026-07-23 22:35:52 +0000

    Update Vexta external signer comment

--- src/external_signer.h
+++ src/external_signer.h
@@ -21,7 +21,7 @@ private:
     //! The command which handles interaction with the external signer.
     std::string m_command;
 
-    //! DigiByte mainnet, testnet, etc
+    //! Vexta mainnet, testnet, etc
     std::string m_chain;
 
     const std::string NetworkArg() const;
