9a06ff0f3bd1ef33a95a484d574642462222e67a
Capri <caprimc@yahoo.com>
2026-07-23 22:21:29 +0000

    Update Vexta initialization comment

--- src/init.h
+++ src/init.h
@@ -59,7 +59,7 @@ bool AppInitLockDataDirectory();
  */
 bool AppInitInterfaces(NodeContext& node);
 /**
- * DigiByte core main initialization.
+ * Vexta Core main initialization.
  * @note This should only be done after daemonization. Call Shutdown() if this function fails.
  * @pre Parameters should be parsed and config file should be read, AppInitLockDataDirectory should have been called.
  */
