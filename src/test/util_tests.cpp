9d432229898ec02bbd5cd9653d1e83aa28449a68
Capri <caprimc@yahoo.com>
2026-07-24 13:38:00 +0000

    Update Vexta capitalization test

--- src/test/util_tests.cpp
+++ src/test/util_tests.cpp
@@ -1964,7 +1964,7 @@ BOOST_AUTO_TEST_CASE(test_ToUpper)
 BOOST_AUTO_TEST_CASE(test_Capitalize)
 {
     BOOST_CHECK_EQUAL(Capitalize(""), "");
-    BOOST_CHECK_EQUAL(Capitalize("digibyte"), "Digibyte");
+    BOOST_CHECK_EQUAL(Capitalize("vexta"), "Vexta");
     BOOST_CHECK_EQUAL(Capitalize("\x00\xfe\xff"), "\x00\xfe\xff");
 }
 
